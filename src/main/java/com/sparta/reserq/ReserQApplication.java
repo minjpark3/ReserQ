be83452c2579f4f0ce6148d80bb27338e86c77a2
minjpark3 <minj9850610@gmail.com>
2024-01-24 15:10:15 +0900

    start

---
+++ src/main/java/com/sparta/reserq/ReserQApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.reserq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserQApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/reserq/ReserQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.reserq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
